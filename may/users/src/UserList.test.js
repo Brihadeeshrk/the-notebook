96455aeb69d24cd78ec92b98cda47acc1ee64aad
briha <hello@briha.xyz>
2024-05-10 00:12:10 +0530

    a lot of code duplication, so we separated out the rendering into a separate fn

--- may/users/src/UserList.test.js
+++ may/users/src/UserList.test.js
@@ -1,33 +1,35 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
+const renderComponent = () => {
+  const users = [
+    { name: "Sam", email: "test1@test.com" },
+    { name: "Jane 2", email: "test2@test.com" },
+  ];
+  render(<UserList users={users} />);
+
+  return { users };
+};
+
 test("render the one row per user", () => {
   // render the component, and since we need to pass a list of users in order for this component to work
   //   we'll generate some fake users
-  const users = [
-    { name: "Sam", email: "test@test.com" },
-    { name: "Jane 2", email: "test@test.com" },
-  ];
-  const { container } = render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   // find all the rows in the table
   // const rows = screen.getAllByRole("row");
-  // const rows = within(screen.getByTestId("users")).getAllByRole("row");
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
   //   const table = container.querySelector("table");
 
   //   eslint-disable-next-line
-  const rows = container.querySelectorAll("tbody tr");
+  //   const rows = container.querySelectorAll("tbody tr");
 
   // Assertion: correct number of rows per user
   expect(rows).toHaveLength(users.length);
 });
 
 test("render the email and name of every user", () => {
-  const users = [
-    { name: "Sam", email: "test1@test.com" },
-    { name: "Jane 2", email: "test2@test.com" },
-  ];
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole("cell", { name: user.name });
