0f3f91726f930cf58eb1e2827993219032ef78fe
briha <hello@briha.xyz>
2024-05-25 22:38:04 +0530

    section 8 completed

--- may/codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
+++ may/codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
@@ -13,7 +13,9 @@ function renderComponent() {
     full_name: "briha/project in py",
     language: "js",
     description: "this is a desc",
-    owner: "facebook",
+    owner: {
+      login: "facebook",
+    },
     name: "briha",
     html_url: "https://github.com/facebook/react",
   };
@@ -22,15 +24,39 @@ function renderComponent() {
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+
+  return { repository };
 }
 
 test("to check if the link is being shown in the list item", async () => {
-  renderComponent();
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: /js/i });
+  const link = screen.getByRole("link", { name: /github repository/i });
+  expect(link).toHaveAttribute("href", repository.html_url);
 
   // await act(async () => {
   //   await pause();
   // });
+});
+
+test("shows a fileicon with the appropriate icon", async () => {
+  const { repository } = renderComponent();
+
+  const icon = await screen.findByRole("img", { name: /js/i });
+
+  expect(icon).toHaveClass("js-icon");
+});
+
+test("shows a link to the code editor page", async () => {
+  const { repository } = renderComponent();
+
   await screen.findByRole("img", { name: /js/i });
+  const link = await screen.findByRole("link", {
+    name: new RegExp(repository.owner.login),
+  });
+
+  expect(link).toHaveAttribute("href", `/repositories/${repository.full_name}`);
 });
 
 const pause = () => new Promise((res) => setTimeout(res, 100));
